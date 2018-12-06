2840a78fd905fd62b26962c97e138fd595f039f7
samson_moshe <samson.moshe@gmail.com>
2018-12-06 09:51:21 +0200

    Create main file

---
+++ ex2.py
@@ -0,0 +1,14 @@
+# Moshe Samson <your name> 312297492 <your id>
+import sys
+
+
+def main(args):
+    if len(args) == 4:
+        print(args[0])
+        print(args[1])
+        print(args[2])
+        print(args[3])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
