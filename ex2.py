c1b364a127e15462b421ae0ba36715553af89ac7
samson_moshe <samson.moshe@gmail.com>
2018-12-06 10:22:03 +0200

    receive arguments. Create and Write to output file.

--- ex2.py
+++ ex2.py
@@ -1,13 +1,38 @@
 # Moshe Samson <your name> 312297492 <your id>
 import sys
+lines_counter = 0
+
+
+def create_output_file(filename):
+    f = open(filename, "w")
+    f.write("#Students	Moshe Samson    <your name>  312297492   <your id>" + '\n')
+
+
+def write_to_output(line):
+    global lines_counter
+
+    f = open("output.txt", "a")
+    f.write("#Output" + str(lines_counter) + '\t' + line + '\n')
+    f.close()
+
+    lines_counter += 1
 
 
 def main(args):
-    if len(args) == 4:
-        print(args[0])
-        print(args[1])
-        print(args[2])
-        print(args[3])
+    if len(args) == 5:
+        devl_filename = args[1]
+        test_filename = args[2]
+        input_word = args[3]
+        output_filename = args[4]
+    else:
+        print("Not enough arguments! Exiting...")
+        exit(-1)
+
+    create_output_file(output_filename)
+    write_to_output(devl_filename)
+    write_to_output(test_filename)
+    write_to_output(input_word)
+    write_to_output(output_filename)
 
 
 if __name__ == "__main__":
