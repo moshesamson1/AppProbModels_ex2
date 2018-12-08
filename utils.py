72c8ec6c78f0544eb582b5b336fd6ec892c81ae0
Roxnya <chernroni@gmail.com>
2018-12-08 17:33:53 +0200

    for now returned utils to split file without random & some fixes

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def pre_process_set(file):
         content = f.readlines()
     # iterate over every 4th line - only content without headers and \n
     for line_idx in range(2, len(content), 4):
-        articles_content.append(content[line_idx])
+        articles_content.extend(content[line_idx].split())
         words = content[line_idx].split()
         for w in words:
             voc[w] = voc.setdefault(w, 0) + 1
@@ -38,30 +38,34 @@ def split_set(content, split_amount, total_count):
     :param total_count: total number of events in given content
     :return: train set and validation set and their word-counting dictionaries
     '''
-    assert split_amount > 1 or split_amount <= 0, "split_set parameter split_amount is out of range"
-    train_size = math.round(split_amount * split_amount)
+    assert split_amount <= 1 and split_amount > 0, "split_set parameter split_amount is out of range"
+    train_size = round(total_count * split_amount)
     # TODO: CHECK if val and train sets are needed as array or as numbers
     train_set, val_set = [], []
     train_counter, train_fr, val_counter = dict(), dict(), dict()
 
     # do random split
-    shuffled_rows = random.shuffle(content)
-    for line_idx in range(len(train_size)):
-        words = shuffled_rows[line_idx].split()
+    #shuffled_rows = content.copy()
+    #random.shuffle(shuffled_rows)
+    for word in content:
+        #words = line.split()
         # if train set array reached desired value, add to validation array, otherwise add to train array
         if len(train_set) == train_size:
-            val_set.extend(words)
-            add_encountered_words(words, val_counter)
+            val_set.append(word)
+            #add_encountered_words(word, val_counter)
+            val_counter[word] = val_counter.setdefault(word, 0) + 1
         else:
-            train_set.extend(words)
-            add_encountered_words(words, train_counter)
-    assert len(train_set) != train_size or len(val_set) != total_count - train_size
+            train_set.append(word)
+            #add_encountered_words(word, train_counter)
+            train_counter[word] = train_counter.setdefault(word, 0) + 1
+    assert len(train_set) == train_size and len(val_set) == total_count - train_size
 
     # create map of number of encounters -> words for train
     for word, freq in train_counter.items():
-        train_fr[freq] = train_fr.setdefault(freq, []).append(word)
+        train_fr.setdefault(freq, []).append(word)
     # add zero frequencies that appeared in validation.
+    train_fr[0] = []
     for word,freq in val_counter.items():
         if word not in train_counter:
-            train_fr[0] = train_fr.setdefault(freq, []).append(word)
+            train_fr[0].append(word)
     return train_set, val_set, train_counter, val_counter, train_fr
