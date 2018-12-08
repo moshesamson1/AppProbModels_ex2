ff2658300ef8c4c7a57a82588f66b9387cddeefa
Roxnya <chernroni@gmail.com>
2018-12-09 00:22:26 +0200

    added function comments

--- held_out.py
+++ held_out.py
@@ -4,12 +4,24 @@ import math
 
 class HeldOut:
     def __init__(self, dev_file, voc_size):
+        '''
+        Initializes Held Out model with given develop file and assumed vocabulary size
+        :param dev_file: develop file to process
+        :param voc_size: assumed vocabulary size
+        '''
         voc, total_count, articles_content = ul.pre_process_set(dev_file)
         self.train_set, self.val_set, self.train_counter, self.val_counter, self.train_fr = ul.split_set(
             articles_content, 0.5, total_count)
         self.VOC_SIZE = voc_size
 
     def calc_ho_probability(self, word, fed_frequency=None):
+        '''
+        Calculates given words discounted probability.
+        If fed_frequency parameter is passed, calculates the discounted probability of r = fed_frequency
+        :param word: word for which probability will be calculated
+        :param fed_frequency: if this parameter is set - calculates probability for r = fed_frequency
+        :return: calculated probability
+        '''
         if fed_frequency is None:
             w_fr = 0 if word not in self.train_counter else self.train_counter[word]
         else:
@@ -20,10 +32,21 @@ class HeldOut:
         return nominator / (word_with_w_fr * len(self.val_set))
 
     def calc_ho_nominator(self, w_fr):
+        '''
+        Calculates t_r - nominator in the probability formula
+        Calculates the sum of held out(validation) frequencies for all the words with w_fr frequency in training set
+        :param w_fr: frequency to calculate
+        :return: calculated nominator
+        '''
         # if w's frequency is 0: all the words we did not see during training and did see during val contribute to the sum
         return sum([self.val_counter.get(w, 0) for w in self.train_fr[w_fr]])
 
     def calc_perplexity(self, test_file):
+        '''
+        Calculates models perplexity over test set
+        :param test_file:
+        :return: calculated perplexity
+        '''
         voc, total_count, articles_content = ul.pre_process_set(test_file)
         sum=0
         for w in voc.keys():
